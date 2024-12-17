5ba388eb08537786101469b86c7ce379cfad3a7d
keivnjs <kjsurtan@gmail.com>
2024-12-17 14:18:06 +0700

    init commit

---
+++ xplr/init.lua
@@ -0,0 +1,25 @@
+-- https://xplr.dev/en/configuration
+
+---@diagnostic disable
+version = "0.21.3"
+local xplr = xplr
+---@diagnostic enable
+
+local home = os.getenv("HOME")
+package.path = home .. "/.config/xplr/plugins/?/init.lua;" .. home .. "/.config/xplr/plugins/?.lua;" .. package.path
+
+-- Plugin Manager
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
+local xpm_url = "https://github.com/dtomvan/xpm.xplr"
+
+package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/init.lua"
+
+os.execute(string.format("[ -e '%s' ] || git clone '%s' '%s'", xpm_path, xpm_url, xpm_path))
+
+-- Plugins
+require("xpm").setup({
+	"gitlab:hartan/web-devicons.xplr",
+})
+
+require("zentable").setup()
+require("icons").setup()
