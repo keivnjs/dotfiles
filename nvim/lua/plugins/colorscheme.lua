3aab528fbddf6c63f7283c161cfb2e95e39436b1
keivnjs <kjsurtan@gmail.com>
2025-06-24 15:15:30 +0700

    fix: change colorscheme to sonokai

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-    }
-  end,
+	{
+		"sainnhe/sonokai",
+		priority = 1000,
+		config = function()
+			vim.g.sonokai_transparent_background = "1"
+			vim.g.sonokai_enable_italic = "1"
+			vim.g.sonokai_style = "maia"
+			vim.cmd.colorscheme("sonokai")
+		end,
+	},
 }
