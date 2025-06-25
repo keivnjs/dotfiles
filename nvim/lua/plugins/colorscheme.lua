5af224891bbc039553bd5460d2356c70cb07d428
keivnjs <kjsurtan@gmail.com>
2025-06-25 17:09:56 +0700

    fix: change colorscheme to tokyonight

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,10 @@
 return {
-	{
-		"sainnhe/sonokai",
-		priority = 1000,
-		config = function()
-			vim.g.sonokai_transparent_background = "1"
-			vim.g.sonokai_enable_italic = "1"
-			vim.g.sonokai_style = "maia"
-			vim.cmd.colorscheme("sonokai")
-		end,
+	"folke/tokyonight.nvim",
+	opts = {
+		transparent = true,
+		styles = {
+			sidebars = "transparent",
+			floats = "transparent",
+		},
 	},
 }
